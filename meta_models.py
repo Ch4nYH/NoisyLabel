a450a6e3fb6827f0c136cdc35db99ec2be60bc22
hfercc <hfercxx@hotmail.com>
2020-03-17 21:32:07 +0800

    p

--- meta_models.py
+++ meta_models.py
@@ -343,27 +343,38 @@ class Model(MetaModule):
     def __init__(self, num_classes = 10, input_channel = 3):
         super(Model, self).__init__()
 
-        self.feature = nn.Sequential(
-            MetaConv2d(input_channel, 128, 3, padding = 2),
-            MetaConv2d(128, 128, 3, padding = 2),
-            MetaConv2d(128, 128, 3, padding = 2),
-            nn.MaxPool2d(2, stride = 2),
-            nn.Dropout(p = 0.25),
-            MetaConv2d(128, 256, 3, padding = 2),
-            MetaConv2d(256, 256, 3, padding = 2),
-            MetaConv2d(256, 256, 3, padding = 2),
-            nn.MaxPool2d(2, stride = 2),
-            nn.Dropout(p = 0.25),
-            MetaConv2d(256, 512, 3, padding = 2),
-            MetaConv2d(512, 256, 3, padding = 2),
-            MetaConv2d(256, 128, 3, padding = 2),
-            nn.AdaptiveAvgPool2d((1,1)),
-        )
+        
+        self.conv1 = MetaConv2d(input_channel, 128, 3, padding = 2),
+        self.conv2 = MetaConv2d(128, 128, 3, padding = 2),
+        self.conv3 = MetaConv2d(128, 128, 3, padding = 2),
+        #elf.conv1 = nn.MaxPool2d(2, stride = 2),
+        #self.conv1 = nn.Dropout(p = 0.25),
+        self.conv4 = MetaConv2d(128, 256, 3, padding = 2),
+        self.conv5 = MetaConv2d(256, 256, 3, padding = 2),
+        self.conv6 = MetaConv2d(256, 256, 3, padding = 2),
+        #self.conv1 = nn.MaxPool2d(2, stride = 2),
+        #self.conv1 = nn.Dropout(p = 0.25),
+        self.conv7 = MetaConv2d(256, 512, 3, padding = 2),
+        self.conv8 = MetaConv2d(512, 256, 3, padding = 2),
+        self.conv9 = MetaConv2d(256, 128, 3, padding = 2),
+        
 
         self.classifier = MetaLinear(128, num_classes)
 
-    def forward(self, x):
-        feature = self.feature(x)
-        feature = feature.view(-1, 128)
-        out = self.classifier(feature)
+    def forward(self, x, training = True):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = F.max_pool2d(x, 2, stride = 2)
+        #x = F.dropout(x, training = True)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+        x = F.max_pool2d(x, 2, stride = 2)
+        x = self.conv7(x)
+        x = self.conv8(x)
+        x = self.conv9(x)
+        x = F.adaptive_avg_pool2d(x, 1)
+        x = x.view(-1, 128)
+        out = self.classifier(x)
         return out
