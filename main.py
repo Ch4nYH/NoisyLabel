b1e9f43be4b3e11e77fb30c08c888aa67a8395e1
hfercc <hfercxx@hotmail.com>
2020-03-17 23:53:32 +0800

    p

--- main.py
+++ main.py
@@ -100,7 +100,8 @@ def train(model, input_channel, optimizer, criterion, train_loader, val_loader, 
         if index % 100 == 0:
             print("[{}/{}] Positive: {}, Negative: {}" .format(index, len(train_loader), torch.sum(grad_eps > 0), torch.sum(grad_eps < 0)))
         index += 1
-        norm_c = torch.sum(abs(grad_eps))
+        w = torch.clamp(-grad_eps, min = 0)
+        norm_c = torch.sum(w)
 
         w = grad_eps / norm_c
         print(w)
