e20f4674e83fe3b40eff86409c0b09fae9369fbb
Ch4nYH <hfercxx@hotmail.com>
2020-04-29 14:38:19 +0800

    add param

--- run_cifar10.sh
+++ run_cifar10.sh
@@ -6,12 +6,13 @@ arch=$3
 gpu=$4
 clamp=$5
 epoch=100
-gamma=0.05
+gamma=1
+batch_size=128
 
 if [[ $clamp -eq  1 ]]
 then
-    nohup python main.py -m ${model_name} -a ${arch} -d cifar10 -c all ${extra_components} -e 100 -g ${gpu} --clamp > ${model_name}_clamp_cifar10.out &
+    nohup python main.py -m ${model_name} -a ${arch} -d cifar10 -c all ${extra_components} -e ${epoch} -g ${gpu} --clamp -b ${batch_size} --gamma ${gamma}> ${model_name}_clamp_cifar10.out &
 else 
-    nohup python main.py -m ${model_name} -a ${arch} -d cifar10 -c all ${extra_components} -e 100 -g ${gpu} > ${model_name}_cifar10.out &
+    nohup python main.py -m ${model_name} -a ${arch} -d cifar10 -c all ${extra_components} -e ${epoch} -g ${gpu} -b ${batch_size} --gamma ${gamma} > ${model_name}_cifar10.out &
 fi
 
