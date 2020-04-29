1289ee38d2b5906cebe77f2bb3424a7ad037050b
Ch4nYH <hfercxx@hotmail.com>
2020-04-29 14:39:20 +0800

    fixed

--- run_cifar10.sh
+++ run_cifar10.sh
@@ -9,7 +9,7 @@ epoch=100
 gamma=1
 batch_size=128
 
-if [[ $clamp -eq  1 ]]
+if [[ ${clamp} -eq  1 ]]
 then
     nohup python main.py -m ${model_name} -a ${arch} -d cifar10 -c all ${extra_components} -e ${epoch} -g ${gpu} --clamp -b ${batch_size} --gamma ${gamma}> ${model_name}_clamp_cifar10.out &
 else 
