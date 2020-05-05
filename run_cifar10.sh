da1ef64720da772ea0cbbe4a5bfea44af6303a8a
Ch4nYH <hfercxx@hotmail.com>
2020-05-05 13:52:55 +0800

    update

--- run_cifar10.sh
+++ run_cifar10.sh
@@ -3,16 +3,15 @@
 extra_components=$1
 model_name=$2
 arch=$3
-gpu=$4
-clamp=$5
+clamp=$4
 epoch=100
 gamma=1
 batch_size=128
 
 if [[ ${clamp} -eq  1 ]]
 then
-    python main.py -m ${model_name} -a ${arch} -d cifar10 -c all ${extra_components} -e ${epoch} -g ${gpu} --clamp -b ${batch_size} --gamma ${gamma}> ${model_name}_clamp_cifar10.out
+    python main.py -m ${model_name} -a ${arch} -d cifar10 -c all ${extra_components} -e ${epoch} --clamp -b ${batch_size} --gamma ${gamma}> ${model_name}_clamp_cifar10.out
 else 
-    python main.py -m ${model_name} -a ${arch} -d cifar10 -c all ${extra_components} -e ${epoch} -g ${gpu} -b ${batch_size} --gamma ${gamma} > ${model_name}_cifar10.out
+    python main.py -m ${model_name} -a ${arch} -d cifar10 -c all ${extra_components} -e ${epoch} -b ${batch_size} --gamma ${gamma} > ${model_name}_cifar10.out
 fi
 
