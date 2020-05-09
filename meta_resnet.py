4895120d815d069eeee9a913398ccaab9e968d45
Ch4nYH <hfercxx@hotmail.com>
2020-05-09 17:00:09 +0800

    fixed import

--- meta_resnet.py
+++ meta_resnet.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from meta_models import MetaModule, MetaConv2d, MetaBatchNorm2d, MetaLinear, MetaSequential
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
     """3x3 convolution with padding"""
